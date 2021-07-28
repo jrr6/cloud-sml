b32707dde6487296d09f8905528a57f25b27aa77
jrr6 <7482866+jrr6@users.noreply.github.com>
2021-07-28 13:43:42 -0400

    Add (terrible) method to change color theme using Monaco Command Palette

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Editor from "@monaco-editor/react"
 import { language } from "../monaco-config/sml-language"
-import { useColorModeValue } from '@chakra-ui/react'
+import { color, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { ProjectFile } from '../types/projectTypes'
 
 type CodeEditorProps = {
@@ -9,20 +9,43 @@ type CodeEditorProps = {
   onEdit: (newContents: string) => any
 }
 
-export const CodeEditor: React.FC<CodeEditorProps> = ({onEdit, file: { name, contents }}) => (
-  <Editor
-    height="100vh"
-    path={name}
-    defaultLanguage="sml"
-    defaultValue={contents}
-    options={{tabSize: 2}}
-    onMount={(editor, monaco) => {
-      monaco.languages.register({ id: "sml" })
-      // language IS IMonarchLanguage, I just can't figure out how to import the types...
-      // @ts-ignore
-      monaco.languages.setMonarchTokensProvider("sml", language)
-    }}
-    onChange={(newContents, _) => onEdit(newContents || "")}
-    theme={useColorModeValue("vs", "vs-dark")}
-  />
-)
+export const CodeEditor: React.FC<CodeEditorProps> = ({onEdit, file: { name, contents }}) => {
+  const [colorChanger, setColorChanger] = React.useState(false)
+
+  // This is a terrible approach...but none of the good ones will work
+  // (can't call toggleColorMode as action.run b/c doesn't update after the initial color change)
+  const ColorChangerDummy: React.FC<{refresh: boolean}> = ({ refresh }) => {
+    const { toggleColorMode } = useColorMode()
+    React.useEffect(() => { if (refresh) { toggleColorMode(); setColorChanger(false) }}, [])
+    return (<></>)
+  }
+
+  return (
+    <>
+    <ColorChangerDummy refresh={colorChanger} />
+    <Editor
+      height="100vh"
+      path={name}
+      defaultLanguage="sml"
+      defaultValue={contents}
+      options={{tabSize: 2}}
+      onMount={(editor, monaco) => {
+        // Add SML language support
+        monaco.languages.register({ id: "sml" })
+        // language IS IMonarchLanguage, I just can't figure out how to import the types...
+        // @ts-ignore
+        monaco.languages.setMonarchTokensProvider("sml", language)
+
+        // Add ability to toggle dark/light appearance
+        editor.addAction({
+          id: "change-appearance",
+          label: "Change Color Theme",
+          run: () => { setColorChanger(true) }
+        })
+      }}
+      onChange={(newContents, _) => onEdit(newContents || "")}
+      theme={useColorModeValue("vs", "vs-dark")}
+    />
+    </>
+  )
+}
